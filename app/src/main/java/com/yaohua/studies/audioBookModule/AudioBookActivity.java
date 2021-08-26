2ba0c1c486b1992366ab3f724d7fd4dfb8db79ad
zhangyaohua <1436385860@qq.com>
2021-08-26 15:22:19 +0800

    简述:开启听书模块的开发 类别(需求/bug/优化重构): 影响范围

---
+++ app/src/main/java/com/yaohua/studies/audioBookModule/AudioBookActivity.java
@@ -0,0 +1,32 @@
+package com.yaohua.studies.audioBookModule;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import com.yaohua.studies.R;
+import com.yaohua.studies.utils.WeakReferenceHandler;
+
+public class AudioBookActivity extends AppCompatActivity implements Handler.Callback {
+
+    public static final String TAG = "AudioBookPlayLog";
+    protected WeakReferenceHandler mHandler;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_audio_book);
+        Log.i(TAG,"onCreate");
+
+        mHandler = new WeakReferenceHandler(this);
+    }
+
+    @Override
+    public boolean handleMessage(@NonNull Message message) {
+        return false;
+    }
+}
