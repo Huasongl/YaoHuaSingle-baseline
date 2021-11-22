c23cfa1249c69a0e90e4efc0a0660dd6854d731f
zhangyaohua <1436385860@qq.com>
2021-11-22 14:03:32 +0800

    简述:查找数组中比左边大右边小的的数 类别(需求/bug/优化重构): 影响范围

--- app/src/main/java/com/yaohua/studies/javabase/SerializableText.java
+++ app/src/main/java/com/yaohua/studies/javabase/SerializableText.java
@@ -8,6 +8,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -15,7 +16,31 @@ import java.util.PriorityQueue;
 public class SerializableText {
     private static final String PATH = "./super.bin";
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        int dTestArray[] = {1,8,6,9,10,15,12,20};
+        ArrayList<Integer> res = new ArrayList<>();
+        findThePivotElement(dTestArray,dTestArray.length,res);
+        System.out.println(res);
+    }
+
+    private static void findThePivotElement(int[] data, int len, ArrayList<Integer> res) {
+        int[] tempArray = new int[len];
+        int r_min = data[len-1];
+        for(int i = len-1;i>=0;--i){
+            if(data[i]<r_min){
+                r_min = data[i];
+            }
+            tempArray[i] = r_min;
+        }
+        int l_max = data[0];
+        for (int i = 0;i<len-1;++i){
+            if(data[i]>l_max){
+                l_max = data[i];
+                if(data[i]<tempArray[i+1]){
+                    res.add(data[i]);
+                }
+            }
+        }
     }
 
 }
